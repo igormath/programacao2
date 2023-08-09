8a22977b4f618f586a115960367a438fc89532a6
igormath <igorxt9@gmail.com>
2023-08-09 09:36:32 -0300

    Update App and Venda classes

--- exercicio_farmacia/farmacia/src/entities/Venda.java
+++ exercicio_farmacia/farmacia/src/entities/Venda.java
@@ -6,8 +6,6 @@ public class Venda {
 
     public double totalValue(Produto produto, int quantity){
         produto.setStockQnt(produto.getStockQnt() - quantity);
-        System.out.println(produto.getPrice());
-        System.out.println(produto.getDiscount());
         return cartPrice += quantity * (produto.getPrice() * (1 - (produto.getDiscount() / 100)));
     }
 
